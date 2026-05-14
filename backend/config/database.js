b8f5d6b333e6364a8faec787afcdc24e51471d79
20P35A0121 <sivakotinallamilli1627@gmail.com>
2026-05-14 09:33:24 +0530

    fix sqlite persistence

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "/tmp/database.sqlite",
+  storage: "./database.sqlite",
   logging: false,
 });
 
